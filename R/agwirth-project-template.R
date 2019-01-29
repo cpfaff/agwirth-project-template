b08d7d22de1c342fc9232353f0ef6d3e2739c328
cpfaff <ctpfaff@arcor.de>
2019-01-29 11:57:44 +0100

    Try to fix code execution error

--- R/agwirth-project-template.R
+++ R/agwirth-project-template.R
@@ -1,10 +1,3 @@
-# This function showcases how one might write a function to be used as an
-# RStudio project template. This function will be called when the user invokes
-# the New Project wizard using the project template defined in the template file
-# at:
-#
-#   inst/rstudio/templates/project/hello_world.dcf
-#
 # The function itself just echos its inputs and outputs to a file called INDEX,
 # which is then opened by RStudio when the new project is opened.
 generate_template <- function(path, ...) {
